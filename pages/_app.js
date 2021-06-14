5c20466b5a7d1a58702dbf5f3da94fcc3444ab2c
anogray <bbncr97@gmail.com>
2021-06-15 02:41:39 +0530

    updated 6 jun carousel size

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,15 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <style jsx global>{`
+  body {
+    background: #e7f9e773;
+  }
+`}</style>
+  </>)
 }
 
 export default MyApp
